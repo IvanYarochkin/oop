5cc987bbda13d3538b1e33ac5516bca6046a4ff4
IvanYarochkin <Ivan.Yarochkin97@gmail.com>
2017-10-21 12:47:13 +0300

    Create writer class

---
+++ src/main/java/by/tc/task01/dao/reader/Reader.java
@@ -0,0 +1,43 @@
+package by.tc.task01.dao.reader;
+
+import org.apache.logging.log4j.util.Strings;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class Reader {
+
+    private final String FILE_PATH = "src/main/resources/appliances_db.txt";
+
+    private Reader() {
+    }
+
+    private static class SingletonHolder {
+        private static final Reader INSTANCE = new Reader();
+    }
+
+    public static Reader getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+
+    public String getFilePath() {
+        return FILE_PATH;
+    }
+
+    public List<String> readFromFile() {
+        List<String> lines = new ArrayList<>();
+        try {
+            lines = Files.lines(Paths.get(FILE_PATH))
+                    .filter(Strings::isNotEmpty)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+}
